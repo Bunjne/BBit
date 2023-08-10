d554d6748cf3085f7d1cb04b58be685be33b93da
Thanapol Sopanhari <thanapol.sopanhari@7peakssoftware.com>
2023-08-10 11:38:18 +0700

    feat: project initialization

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "BBit"
+include(":androidApp")
+include(":shared")
---
+++ shared/src/commonMain/kotlin/com/bunjne/bbit/Greeting.kt
@@ -0,0 +1,9 @@
+package com.bunjne.bbit
+
+class Greeting {
+    private val platform: Platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
