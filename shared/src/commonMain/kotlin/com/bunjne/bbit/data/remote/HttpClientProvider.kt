7213d6904c5e1573010f0dac1569cb33a4d70ebc
Thanapol Sopanhari <thanapol.sopanhari@7peakssoftware.com>
2024-03-05 13:47:08 +0700

    fix: remove CIO and choose HTTP client by default

--- shared/src/commonMain/kotlin/com/bunjne/bbit/data/remote/HttpClientProvider.kt
+++ shared/src/commonMain/kotlin/com/bunjne/bbit/data/remote/HttpClientProvider.kt
@@ -1,19 +1,19 @@
 package com.bunjne.bbit.data.remote
 
 import io.ktor.client.HttpClient
-import io.ktor.client.engine.cio.CIO
 import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
-import io.ktor.client.plugins.logging.DEFAULT
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.plugins.logging.SIMPLE
 import io.ktor.http.HttpHeaders
 import io.ktor.http.URLBuilder
 import io.ktor.http.URLProtocol
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
+
 
 
 class HttpClientProvider(
@@ -25,7 +25,7 @@ class HttpClientProvider(
 //            json(json)
 //        }
 //    }
-    fun provide() = HttpClient(CIO) {
+    fun provide() = HttpClient {
         install(ContentNegotiation) {
             json(json)
         }
